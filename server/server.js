57005d3aab9ead61057472961a7f4e27ab40119d
petestapp <63821853+petestapp@users.noreply.github.com>
2021-09-25 16:06:11 -0500

    route set up and buttons put in html

--- server/server.js
+++ server/server.js
@@ -7,8 +7,13 @@ const app = express();
 app.use(express.static(`server/public`));
 // globals
 const port = 5000;
+let calculations = [];
 // spin up server
 app.listen(port, () =>{
     console.log(`server is up on:`, port);
 })
 // routes
+app.get(`/calculator`, (req, res)=>{
+    console.log(`/calculator GET hit`);
+    res.send(calculations);
+})
