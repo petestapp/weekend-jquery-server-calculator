3905eaaf5996cbda005241c39803a5af4910ae97
petestapp <63821853+petestapp@users.noreply.github.com>
2021-09-27 14:04:15 -0500

    inputs working to add to an object in sendInput()

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,16 @@
 $(document).ready(onReady);
 
+let operator
+
 function onReady(){
     getCalculations();
+    $(`#addButton`).on(`click`, setOperationToAdd);
+    $(`#subtractButton`).on(`click`, setOperationToSubtract);
+    $(`#multiplyButton`).on(`click`, setOperationToMultiply);
+    $(`#divideButton`).on(`click`, setOperationToDivide);
+    $(`#equalsButton`).on(`click`, sendInput);
+    // $(`#clearButton`).on(`click`, clearCalculator);
+
 }
 
 function getCalculations(){
@@ -10,6 +19,40 @@ function getCalculations(){
         method: `GET`,
         url: `/calculations`
     }).then(function(response){
-        console.log(`back from GET`)
+        console.log(`back from GET:`, response);
+    }).catch(function(err){
+        alert(`error`);
+        console.log(err);
     })
 }
+
+function setOperationToAdd(){
+    console.log(`in setOperationToAdd()`);
+    operator = `+`;
+}
+
+function setOperationToSubtract(){
+    console.log(`in setOperationToSubtract()`);
+    operator = `-`;
+}
+
+function setOperationToMultiply(){
+    console.log(`in setOperationToMultiply()`);
+    operator = `*`
+}
+
+function setOperationToDivide(){
+    console.log(`in setOperationToDivide()`);
+    operator = `/`
+}
+
+function sendInput(){
+    console.log(`in sendInput()`);
+    let rawDataObject = {
+        firstOperand: $(`#firstOperandIn`).val(),
+        operation: operator,
+        secondOperand: $(`#secondOperandIn`).val()
+    }
+    console.log(`rawDataObject:`, rawDataObject);
+}
+
